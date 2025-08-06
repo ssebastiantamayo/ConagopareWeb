c5d980c45f1ebfcf6ccab48060db7c809ac87212
ssebastiantamayo <ssebastiantamayo@gmail.com>
2025-08-05 19:25:50 -0500

    Create page.tsx

---
+++ src/app/Articulos/Entre el fogón y el espacio digital: Relaciones intergeneracionales en la ruralidad actual /page.tsx
@@ -0,0 +1,236 @@
+import Link from "next/link";
+
+export default async function ArticuloPage() {
+
+  return (
+    <div className="max-w-4xl mx-auto bg-gray-100 text-gray-900 p-6 md:p-10 rounded-xl shadow-lg mt-10">
+      <h1 className="text-3xl md:text-4xl font-bold mb-2 font-heading">
+        "Entre el fogón y el espacio digital: Relaciones intergeneracionales en la ruralidad actual"
+      </h1>
+      <p className="text-black-600 font-semibold mb-2 text-left">María Elena Acosta M.</p>
+      <p className="italic mb-6">Profesora Maestría de Estudios Urbanos<br />FLACSO Ecuador</p>
+
+      <article className="space-y-6 text-justify leading-relaxed text-gray-800 whitespace-pre-wrap">
+        <p>
+          <b>Introducción</b>
+          {"\n\n"}
+          Este artículo explora cómo personas de distintas generaciones de la ruralidad viven las transformaciones 
+          del territorio y de las ontologías relacionales en la vida cotidiana, que se expresan entre los relatos 
+          orales territoriales alrededor del fogón (el fuego) y las narrativas en el espacio digital a 
+          través de las redes sociales. 
+          {"\n\n"}
+          Propone enriquecer las miradas sobre cómo se producen, transmiten y transforman las memorias rurales 
+          desde la oralidad territorial alrededor del fogón en las comunidades y cómo a partir del espacio 
+          digital se conectan con el mundo trasmitiendo los significantes culturales o intereses particulares. 
+          En los dos casos, cómo sus sentires, sus modos de vida y sus tradiciones se reflejan en las formas 
+          de contar, recordar y resistir a través de relatos intergeneracionales. Reconociendo el valor de la 
+          historia, la memoria y los saberes, como diría Sousa Santos, para “no caer en el desperdicio de 
+          la experiencia” (2003), ni correr el riesgo de desechar múltiples formas de conocimiento y distintas 
+          formas de contar.  Reconociendo la existencia de una interculturalidad epistémica, saberes propios 
+          de cada cultura y de distintas generaciones.
+          {"\n\n"}
+          El presente artículo se enfoca en al menos dos generaciones están presentes y cohabitan los 
+          territorios rurales, la juventud y quienes están en la adultez mayor, la primera genera relatos 
+          de vida a partir del uso de los medios digitales, diversas redes sociales y la segunda lo hace 
+          a través de la oralidad territorial, narrativas alrededor del fogón, de los espacios comunes, 
+          surgiendo manifestaciones de sincretismo y de hibridación cultural. Asumiendo como sincretismo 
+          la existencia de manifestaciones propias que se entrelazan, en este caso de las formas de 
+          compartir las tradiciones rurales, de diferentes generaciones entre lo oral territorial 
+          comunitario y lo digital redes sociales; y como hibridación, donde coexisten manifestaciones
+          particulares a partir del fogón y lo digital sin relacionarse, muchas entre la tensión o 
+          la indiferencia.
+          {"\n\n"}
+          Cohabitar el territorio rural desde la oralidad territorial y desde las narrativas digitales, 
+          muchas veces pasa por una oscilación identitaria , que, en este caso, a veces llevaría 
+          más hacia lo digital y otras a la convivencia alrededor del fogón, identidad oscilante 
+          entre la narrativa territorial y la digital. Lo requiere conocer y reconocer las 
+          relaciones socioculturales intergeneracionales, que entrecruza la memoria, el territorio
+          y la tecnología en el mundo rural actual. 
+          {"\n\n"}
+          Se comparte reflexiones a partir de una mirada crítica sobre la transformación digital, 
+          sin perder de vista lo simbólico alrededor del fogón como representación del sentido de 
+          comunidad. Siendo importante reconocer que habitan voces diferenciadas, pero que requieren
+          espacios de reconocimiento mutuo, donde se observa el dualismo cultural y prácticas distintas. 
+          Lo digital y la oralidad territorial, aparentemente opuestos, coexisten y se influyen mutuamente.
+          {"\n\n"}
+          <b>El fogón significante en la ruralidad</b>
+          {"\n\n"}
+          En el mundo rural, desde diferentes denominaciones, el fogón, la tulpa, el brasero, 
+          ha sido una práctica social cargada de simbolismo cultural (HAAL 2023) . Más que un elemento 
+          funcional, es un espacio multifacético que une. Se ha constituido en un lenguaje que facilita 
+          las relaciones entre seres humanos, la naturaleza y el entorno construido. Conecta la naturaleza 
+          y la espiritualidad, contribuyendo a la construcción de su identidad rural.
+          {"\n\n"}
+          Por tanto, el fogón es mucho más que un simple lugar para cocinar; representa un centro de 
+          la vida familiar y comunitario, un espacio de reunión y transmisión de tradiciones , así como 
+          de modos de vida, conocimientos, espiritualidad, pero sobre todo de saberes y visión del mundo, 
+          en un entramado del sentido de comunidad de trasmisión intergeneracional, preservando la identidad.
+          {"\n\n"}
+          Según Gutiérrez (2019), a través de cuentos, mitos, leyendas y canciones transmitidas de 
+          boca en boca las sociedades han mantenido viva su historia colectiva y han transmitido sus 
+          valores fundamentales a las generaciones futuras. A través de la participación en rituales, 
+          ceremonias y conversaciones compartidas los miembros de una comunidad establecen vínculos 
+          sociales y desarrollan un sentido de solidaridad y pertenencia. Por ejemplo, en los territorios 
+          rurales de la costa se encuentra los amorfinos, en la sierra los Raymi 
+          (Inti Raymi, Killa Raymi, Pawkar Raymi), entre otros.
+          <b>Las narrativas digitales desde la ruralidad</b>
+          {"\n\n"}
+          Si bien se insiste en evidenciar las brechas en el acceso al mundo digital y a la tecnología, 
+          según datos, el 81% de los habitantes de zonas urbanas a nivel mundial utilizaban internet, 
+          en comparación con sólo el 50% de la población en áreas rurales (UIT 2023). 
+          El uso de redes sociales en los territorios rurales es un hecho. En Ecuador de los 17,5 millones 
+          de habitantes, 13 millones utilizan las redes sociales. Hay 15,6 millones de líneas de 
+          telefonía móvil en el país y 9,1 millones de personas acceden a las redes sociales 
+          desde dispositivos móviles. Fa-cebook fue utilizado por 12 millones de personas en 
+          el país en enero de 2020; la siguiente plataforma más popular fue Instagram, con 
+          cuatro millones de usuarios (Del Alcázar 2020). 
+          {"\n\n"}
+          Repasando varios estudios y reportajes sobre el uso de redes sociales en la ruralidad, 
+          se observan dos tendencias, por una parte, su uso para la afirmación identitaria, 
+          “el uso de las redes sociales brinda nuevas oportunidades para expresar nuestra identidad al 
+          mundo”; “en plataformas como Instagram, Facebook y TikTok las personas jóvenes pueden 
+          compartir sus experiencias, tradiciones y cultura, promoviendo el orgullo por sus raíces 
+          y generando una visibilidad positiva de la vida rural”.
+          {"\n\n"}
+          Por otra parte, las redes sociales se usan como el medio para la expresión aspiracional, 
+          “reflejan aspiraciones de educación superior y ocupaciones profesionales o técnicas 
+          no agrícolas” ; “las redes sociales también facilitan el acceso a recursos educativos, 
+          tutoriales y noticias que amplían las oportunidades de aprendizaje de las juventudes. 
+          Desde videos sobre técnicas de cultivo hasta tutoriales de marketing digital, estas 
+          plataformas ofrecen contenido relevante y accesible que les permiten aprender 
+          habilidades necesarias para desarrollarse profesionalmente o emprender sus propios proyectos”.
+          {"\n\n"}
+          No obstante, el mundo digital conlleva riesgos, la juventud de territorios rurales 
+          también están expuestos, la falta la una educación digital adecuada los expone a información 
+          falsa, ciberacoso, violación de privacidad, fraude, explotación, dependencia y afectación 
+          a la salud mental, así como el impacto en la cultura, corriendo riego de la pérdida de 
+          interés en las tradiciones locales y la identidad cultural propia, generando una 
+          potencial desconexión con sus raíces.
+          {"\n\n"}
+          Sin embargo, es recurrente encontrar expresión de quienes se definen como 
+          creadores de contenido, resaltando el valor de su cultura a través de las redes 
+          sociales “me veo como representante de mi pueblo kichwa, debemos demostrar la capacidad 
+          que tenemos”, “Trasmitir lo que somos un solo corazón, una solo mete, una sola fuerza”. 
+          Incluso de quienes trasciende las fronteras, que viven en otros países pero que 
+          mantienen nexos con su cultura que expresan “…no hay una manera de ser migrante o 
+          una manera de ser indígena o…”.  O agrupaciones de jóvenes de arte y cultura,
+          quienes también usan las redes para difundir contenidos sobre tradiciones y arte de 
+          sus territorios, así como a través de la autogestión realizar eventos en sus 
+          territorios para afirmar su identidad.
+          {"\n\n"}
+          <b>En síntesis: el fogón y el espacio digital  </b>
+          {"\n\n"}
+          Es importante anotar que las reflexiones presentadas no son generalizables, ni 
+          homogeneizadoras, son algunas pistas que invitan a conocer cómo se imbrica o se 
+          separa la trasmisión sociocultural entre las narrativas territoriales y digitales.
+          {"\n\n"}
+          En los espacios que se generan alrededor del fogón, se manifiestan prácticas recurrentes,
+          que constituyen ritualidades cuyos contenidos son referentes simbólicos que se trasmiten. 
+          La comunidad desde la trasmisión oral territorial y práctica cotidiana recurrentes, 
+          son comunidades donde se genera vínculos, a decir de Hun los rituales son praxis 
+          simbólicas, en la medida que juntan y que engendran alianzas se tramite comunidad (2024). 
+          Sin embargo, el mundo digital ha ido transformando las relaciones, intentando coexistir 
+          con la cultura oral en los territorios rurales que permanece y es duradera. Si bien 
+          lo digital conecta no genera vínculos. Las comunidades digitales son comunidades de 
+          mensajes sin comunidad, generan conexiones, pero no vínculos (Hun 2024). 
+          {"\n\n"}
+          Es posible que lo digital esté poniendo en riesgo la transmisión oral simbólica, 
+          que se trasmite en comunidad. La potencial desaparición de los símbolos referentes 
+          remite a la progresiva atomización de la sociedad. La percepción simbólica desaparece 
+          cada vez más a favor de la percepción serial, que no es capaz de experimentar duración 
+          (Han 2024). Lo serial es la expectativa permanente de lo nuevo, del consumo, 
+          requiere una constante actualización, rechaza lo antiguo, que se tiende a experimentar 
+          en el mundo digital. Incentivando una permanente exigencia de autenticidad, innovación 
+          y creatividad (Han 2024) en la que cada vez más envuelve la llamada modernidad.
+          {"\n\n"}
+          Entonces, por un lado, el compartir alrededor del fogón, genera sentido de comunidad, 
+          sentido del nosotros, a partir de las conversaciones, gestos y símbolos, reforzando 
+          lo colectivo. Por otro lado, el espacio digital tiende al auto escucha, al sentido 
+          del yo, de lo individual, a partir de los “me gusta”, “amigos” y seguidores, 
+          reforzando el ego. Sin embargo, es importante señalar, que, si bien pueden verse 
+          como mundos paralelos en tensión, hay imbricaciones, ensamblajes y cruces, dándose 
+          expresiones de sincretismos, hibridaciones, oscilaciones identitarias entre las 
+          narrativas territoriales y las digitales.
+          {"\n\n"}
+          Por tanto, es necesario reconocer que, a través de la oralidad territorial y del 
+          espacio digital, en las redes sociales se comparten contenidos socioculturales, 
+          manifestaciones identitarias que están latentes en los territorios rurales. 
+          A través generaciones y los distintos rostros que aparecen en las pantallas, en 
+          la actualidad, refleja la multilocalidad, donde las relaciones se mantienen a 
+          través de los medios modernos de comunicación (Dittrich‐Wesbuer et al. 2015), 
+          generando distintas formas de relaciones familiares y comunitarias, así 
+          como sentido de pertenencia, difundiendo, conociendo y apropiándose. 
+          Los riesgos son latentes, pero las transformaciones son eminentes.    
+          {"\n\n"}
+          Se dejan planteadas algunas reflexiones y la invitación a continuar profundizando 
+          sobre el espacio digital en los territorios rurales desde el enfoque intercultural, 
+          que plantea la interacción equitativa de diversas culturas y la posibilidad de 
+          generar expresiones culturales compartidas a través del diálogo y el respeto mutuo, 
+          que permita reconocer las distintas relaciones intergeneracionales actuales que van 
+          surgiendo entre el fogón, la oralidad territorial y el espacio digital a 
+          través de la redes social. 
+          {"\n\n"}
+          <b>Referencias bibliográficas</b>
+          {"\n\n"}
+          Boaventura de Sousa Santos. 2003. <i>Crítica de la razón indolente. Contra el desperdicio de la experiencia</i>. Editorial Desclée De Brouwer, S.A. España.
+          {"\n\n"}
+          Byung-chul Han. 2024. <i>La desaparición de los rituales</i>. Hender Editorial, S.L. Barcelona. 
+          {"\n\n"}
+          Gutiérrez, G. (2019). <i>La palabra hablada: oralidad y cultura en América Latina</i>. Fondo de Cultura Económica.
+          {"\n\n"}
+          Greinke, L; Lange L. 2022. <i>Multilocalidad en zonas rurales: un fenómeno subestimado</i>. <a href="10.1080/21681376.2021.2025417" target="_blank" rel="noopener noreferrer">10.1080/21681376.2021.2025417</a>
+          {"\n\n"}
+          Historia Agraria de América Latina. (2023).  <i>Revista Internacional sobre la historia de las sociedades rurales latinoamericanas</i>. <a href="https://www.haal.cl/index.php/haal/dossier-fire-in-the-rural-world" target="_blank" rel="noopener noreferrer">href="https://www.haal.cl/index.php/haal/dossier-fire-in-the-rural-world</a>
+          {"\n\n"}
+          IICA. 2024. <i>Comunidad de Juventudes Rurales</i> <a href="https://juventudesrurales.iica.int/juventudes-rurales-y-su-acceso-a-las-redes-sociales/" target="_blank" rel="noopener noreferrer">https://juventudesrurales.iica.int/juventudes-rurales-y-su-acceso-a-las-redes-sociales/</a>
+          {"\n\n"}
+          Lowe S, Rod M. 2020. <i>Entendiendo la cultura como una dualidad holárquica dinámica</i>. <a href="https://doi.org/10.1016/j.indmarman.2020.08.013" target="_blank" rel="noopener noreferrer">https://doi.org/10.1016/j.indmarman.2020.08.013</a>
+          {"\n\n"}
+          Ministerio de Educación Nacional. 2017. <i>El Fogón Serie Río de Letras Territorios Narrados</i>. ISBN: 978-958-5424-57-9. Colombia. <a href="https://www.mineducacion.gov.co/1759/articles-360522_foto_portada.pdf" target="_blank" rel="noopener noreferrer">https://www.mineducacion.gov.co/1759/articles-360522_foto_portada.pdf</a>
+          {"\n\n"}
+          Mundo Diners. 2022. <i>3 nuevas influencers indígenas que arrasan en Tik Tok</i>. <a href="https://revistamundodiners.com/mundo-diners-plus/3-indigenas-tik-tok/" target="_blank" rel="noopener noreferrer">https://revistamundodiners.com/mundo-diners-plus/3-indigenas-tik-tok/</a>
+          {"\n\n"}
+          Mujeres indígenas del Ecuador. ----. <a href="https://www.tiktok.com/discover/indijenas-mujeres-del-ecuador" target="_blank" rel="noopener noreferrer">https://www.tiktok.com/discover/indijenas-mujeres-del-ecuador</a>
+          {"\n\n"}
+          Nelson Reascos. 2018. <i>La filosofía como Conciencia Crítica. Conferencia</i>. I Jornadas de Pensamiento Crítico, Filosofía y Teoría. <a href="https://www.youtube.com/watch?v=oSk5a82EJ0g&t=2785s" target="_blank" rel="noopener noreferrer">https://www.youtube.com/watch?v=oSk5a82EJ0g&t=2785s</a>
+          {"\n\n"}
+          Waters, William F., Martin Baca, Shirley Tipanquiza, y Emily McGrath. 2022. <i>Cambio De Identidad Y Uso De Facebook En Adolescentes Y jóvenes indígenas De La Sierra Central Ecuatoriana</i>. Mundos Plurales - Revista Latinoamericana De Políticas Y Acción Pública 9 (2). <a href="https://doi.org/10.17141/mundosplurales.2.2022.5586." target="_blank" rel="noopener noreferrer">https://doi.org/10.17141/mundosplurales.2.2022.5586.</a>
+          {"\n\n"}
+        </p>
+      </article>
+      <div className="mt-16">
+        <h3 className="text-2xl font-bold text-gray-800 mb-6 text-left">Otros artículos recomendados</h3>
+
+        <div className="space-y-4">
+
+          {/* Artículo 1 */}
+          <Link href="/Articulos/articulo-empleo-rural" className="block group">
+            <div className="flex bg-white shadow border border-gray-200 rounded-lg overflow-hidden hover:bg-gray-50 transition">
+              {/*} <img
+          src="/images/difunde/revista2.png"
+          alt="Empleo rural"
+          className="w-24 h-24 object-cover"
+        />{*/}
+              <div className="p-3 flex-1 text-left">
+                <h4 className="text-base font-semibold text-gray-900 leading-snug group-hover:underline mb-1">
+                  El empleo en las parroquias rurales: una deuda estructural
+                </h4>
+                <span className="bg-black text-white text-xs font-bold px-2 py-1 rounded inline-block mb-1">
+                  AUTOR/A 
+                </span>
+                <p className="text-sm text-gray-500 italic">
+                  Análisis de los factores que afectan la generación de empleo en territorios rurales ecuatorianos.
+                </p>
+              </div>
+            </div>
+          </Link>
+
+        </div>
+      </div>
+
+
+
+    </div>
+
+  );
+}
